fcb81a348f43d598738d71a0a50c4a7db72be70a
Jcanotorr06 <jcano062001@gmail.com>
2021-10-11 18:21:24 -0500

    Changed footer text color

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,10 +5,10 @@ const Footer = () => {
         <Box w="100%" py="1rem" bg="transparent" as="footer">
             <Container maxW="container.xl" as="nav">
                 <Flex justifyContent="space-between" alignItems="center">
-                    <Text fontSize="smaller" fontWeight="medium" color="gray.200">
+                    <Text fontSize="smaller" fontWeight="medium" color="gray">
                        © 2021 Nubis - All Rights Reserved
                     </Text>
-                    <Link fontSize="smaller" fontWeight="medium" color="gray.200" href="https://github.com/Jcanotorr06/Nubis" target="_blank" rel="noreferrer">
+                    <Link fontSize="smaller" fontWeight="medium" color="gray" href="https://github.com/Jcanotorr06/Nubis" target="_blank" rel="noreferrer">
                         Github Repo
                     </Link>
                 </Flex>        
